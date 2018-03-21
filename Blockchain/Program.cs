2efcc915c02e75b5f36cab1546874bd38fe5bee5
René Mifsud <renemifsud93@gmail.com>
2018-03-21 20:54:20 +0100

    commtit 1
    commit

---
+++ Blockchain/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NBitcoin;
+
+namespace Blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Key privateKey = new Key();
+           // Console.WriteLine(privateKey);
+
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine(publicKey);
+            Console.WriteLine(publicKey.GetAddress(Network.Main));
+            Console.WriteLine(publicKey.GetAddress(Network.TestNet));
+
+            var publicKeyHash = publicKey.Hash;
+            Console.WriteLine(publicKeyHash);
+        }
+    }
+}
